37cc38fea83ada35bd398c27065a04377463b392
Karim Mohamed <37942217+KarimMohamed20@users.noreply.github.com>
2020-10-29 12:07:29 +0200

    First Commit

---
+++ lib/inappcallalert.dart
@@ -0,0 +1,16 @@
+library inappcallalert;
+
+import 'package:flutter/material.dart';
+
+/// A Call Alert.
+class CallAlert extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      body: Wrap()
+    );
+  }
+
+}
